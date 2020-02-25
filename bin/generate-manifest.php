db237b7de8655741f5d078d1d15dbfba1806c94f
James Nylen <jnylen@gmail.com>
2020-02-25 03:03:56 +0000

    Update manifest script

--- bin/generate-manifest.php
+++ bin/generate-manifest.php
@@ -2,17 +2,15 @@
 <?php
 
 $root = dirname( __DIR__ );
-$repo = 'WP-API/docs';
+$repo = 'ClassicPress/ClassicPress-docs';
 
 $manifest = array();
 $paths = array(
 	$root . '/*.md',
 	$root . '/*/*.md',
-	$root . '/*/*/*.md',
 );
 $excludes = array(
 	$root . '/README.md',
-	$root . '/bin/templates/endpoint.md',
 );
 foreach ( $paths as $path ) {
 	foreach ( glob( $path ) as $file ) {
@@ -21,7 +19,15 @@ foreach ( $paths as $path ) {
 		}
 
 		$slug = basename( $file, '.md' );
-		$key = str_replace( array( $root . '/', '.md' ), '', $file );
+		$filename_base = str_replace( array( $root . '/', '.md' ), '', $file );
+		if ( $slug === 'index' ) {
+			// e.g. 'installing-classicpress/index.md'
+			$slug = basename( dirname( $file ) );
+			$key = str_replace( array( $root . '/', '/index.md' ), '', $file );
+		} else {
+			// e.g. 'browser-support.md'
+			$key = $filename_base;
+		}
 		$parent = null;
 		if ( stripos( $key, '/' ) ) {
 			$bits = explode( '/', $key );
@@ -31,12 +37,22 @@ foreach ( $paths as $path ) {
 		$manifest[ $key ] = array(
 			'slug'            => $slug,
 			'parent'          => $parent,
-			'markdown_source' => sprintf( 'https://github.com/%s/blob/master/%s.md', $repo, $key ),
+			'markdown_source' => sprintf(
+				'https://github.com/%s/blob/master/%s.md',
+				$repo,
+				$filename_base
+			),
 		);
 	}
 }
 
-file_put_contents( $root . '/bin/manifest.json', json_encode( (object) $manifest, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ) );
+file_put_contents(
+	$root . '/bin/manifest.json',
+	json_encode(
+		(object) $manifest,
+		JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+	)
+);
 
 $count = count( $manifest );
 echo "Generated manifest.json of {$count} pages\n";
